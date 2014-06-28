f8318d4abbfd1ae308942285b4a3b8035ced4163
John Buco <johnb77@gmail.com>
2014-06-28 17:57:41 -0400

    canvas setup and testing

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 function AppCtrl($scope) {
   $scope.test = 'hello, world';
+  $scope.test2 = function() {
+    console.log('hello, world 222');
+  };
+
+  var canvas = document.getElementById('grid');
+  canvas.width = 500;
+  canvas.height = 500;
+
+  var ctx = canvas.getContext('2d');
+
+  $scope.draw = function() {
+    ctx.fillText("O", 230, 230); 
+  };
 }
