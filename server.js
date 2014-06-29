4743f326189a1e6d49aa22a24b8dc9ef94325fe5
John Buco <johnb77@gmail.com>
2014-06-29 17:58:13 -0400

    use env port if available

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.get('/favicon.ico', function(req, res){
     res.sendfile('favicon.ico');
 });
 
-var server = app.listen(3000, function() {
-      console.log('Listening on port %d', server.address().port);
+var port = Number(process.env.PORT || 5000);
+var server = app.listen(port, function() {
+    console.log("Listening on " + port);
 });
