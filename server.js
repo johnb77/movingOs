90e468612ae6b32da777801628a7ec814e065c11
John Buco <johnb77@gmail.com>
2014-06-29 17:38:01 -0400

    basic nodejs/express webserver

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.use(express.static(path.join(__dirname, "js")));
+app.get('/hello', function(req, res){
+    res.send('Hello World');
+});
+app.get('/', function(req, res){
+    res.sendfile('index.html');
+});
+app.get('/js/app.js', function(req, res){
+    res.sendfile('js/app.js');
+});
+
+var server = app.listen(3000, function() {
+      console.log('Listening on port %d', server.address().port);
+});
