7aea7f0799eb48a41d174f198e59547e7729b2f6
John Buco <johnb77@gmail.com>
2014-06-29 17:17:47 -0400

    cleanup

--- js/app.js
+++ js/app.js
@@ -1,22 +1,16 @@
 'use strict';
 
 function AppCtrl($scope) {
-  console.log('in AppCtrl');
-  $scope.test = 'hello, world';
-  $scope.test2 = function() {
-    console.log('hello, world 222');
-  };
-
   $scope.draw = function(i) {
     move(i);
   };
 
-  //let's use angular bindings to make the string to move dynamic, default to O
+  //let's use angular bindings to make the string to move, speed, and start index dynamic
   $scope.movingStr = localStorage.getItem('movingStr') || "O";
   $scope.stepSpeed = parseInt(localStorage.getItem('stepSpeed')) || 1000;
   $scope.startIndex = parseInt(localStorage.getItem('startIndex')) || 0;
 
-  //padding so the O doesn't get displayed right on the edge of screen
+  //padding so the movingStr doesn't get displayed right on the edge of screen
   var pad = 50;
 
   //canvas setup
@@ -33,29 +27,15 @@ function AppCtrl($scope) {
     new Point(canvas.width / 2, canvas.height / 2) 
   ];
 
-  //current Point in path
-  var pathIndex = 0;
-
-  //how many steps have been taken
-  var steps = 0;
-
   var growFlag = false;
 
   function move(index) {
-
     clearAllTimeouts();
     var ctx = canvas.getContext('2d');
     ctx.font = '20pt Calibri';
-
-    //ctx.fillText("O", 230, 230); 
-    //console.log('in move:' + new Date());
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     var point = defPath[index % defPath.length];
     ctx.fillText($scope.movingStr, point.x, point.y);
-    steps++;
-    //if ((steps - 1) % defPath.length === 0) {
-    console.log('growflag: ' + growFlag);
-    console.log('point: ' + JSON.stringify(point));
     if (growFlag && point.equals(defPath[defPath.length - 1])) {
       //grow:  O --> 00
       $scope.movingStr += $scope.movingStr[$scope.movingStr.length - 1];
@@ -64,7 +44,7 @@ function AppCtrl($scope) {
     localStorage.setItem("startIndex", index);
     localStorage.setItem("stepSpeed", $scope.stepSpeed);
     localStorage.setItem("movingStr", $scope.movingStr);
-    setTimeout(move, parseInt($scope.stepSpeed), index + 1); //TODO: timeout should be user definable
+    setTimeout(move, parseInt($scope.stepSpeed), index + 1); 
   }
 
   //constructor for Point object
