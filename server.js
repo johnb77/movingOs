c4be040e6c568fb9bf3a9d8ebe61aefc6d58982c
John Buco <johnb77@gmail.com>
2014-07-01 17:23:35 -0400

    cleanup server.js

--- server.js
+++ server.js
@@ -2,15 +2,14 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.use(express.static(path.join(__dirname, "js")));
-app.get('/hello', function(req, res){
-    res.send('Hello World');
-});
 app.get('/', function(req, res){
     res.sendfile('index.html');
 });
 app.get('/js/app.js', function(req, res){
     res.sendfile('js/app.js');
+});
+app.get('/css/app.css', function(req, res){
+    res.sendfile('css/app.css');
 });
 app.get('/favicon.ico', function(req, res){
     res.sendfile('favicon.ico');
